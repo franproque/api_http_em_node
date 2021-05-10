5944a57745b096e1859a58cdc989fc7b90b0ce07
unknown <franproque>
2021-05-10 16:24:58 -0300

    Auth JWT

--- index.js
+++ index.js
@@ -3,9 +3,41 @@ const express = require('express');
 const app= express()
 
 const bodyParser= require("body-parser");
+const jwt = require("jsonwebtoken");
+
+const JWTSecret = "Francisco e gostoso pra caralho"
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
+
+
+function auth(req,res,next)
+{
+    const authToken = req.headers['authorization'];
+    console.log(authToken);
+    if(authToken!=undefined)
+    {
+        const bearer = authToken.split(' ');
+        var token =bearer[1]
+        jwt.verify(token,JWTSecret,(err,data)=>{
+            if(err)
+            {
+                res.status(401)
+                res.json({err:"Token inválido"})
+            }else{
+                req.token = token;
+                req.loggedUser = {id:data.id,email:data.email}
+                next();
+            }
+        })
+        console.log(bearer)
+    }else{
+        res.status(401);
+        res.json({err:"Token inválido"})
+    }
+  
+}
+
 
 var DB = {
     games:[
@@ -27,10 +59,25 @@ var DB = {
             year:2012,
             price:20
         }
+    ],
+    users:[
+        {
+            id:1,
+            name:"Francisco Piloto Roque",
+            email:"franroque@gmail.com",
+            pass:1234
+        },
+        {
+            id:20,
+            name:"Guilherme",
+            email:"guilherme@gmail.com",
+            pass:1234
+        }
+
     ]
 }
 
-app.get("/games",(req,res)=>{
+app.get("/games",auth,(req,res)=>{
     res.statusCode=200;
     res.json(DB.games);
 });
@@ -106,6 +153,42 @@ app.put("/games/:id",(req,res)=>{
         }else{
             res.sendStatus(404)
         }
+    }
+})
+
+app.post("/auth",(req,res)=>{
+    var {email, pass} = req.body
+   
+    if(email!=undefined)
+    {
+       var user = DB.users.find(u=> u.email==email)
+        if(user!=undefined)
+        {
+            if(user.pass == pass)
+            {
+                jwt.sign({id:user.id,email:user.email},JWTSecret,{expiresIn:'48h'}, (err,token)=>{
+                    if(err)
+                    {
+                        res.status(400)
+                        res.json({err:"Flha Interna"})
+                    }else{
+                        res.status(200)
+                        res.json({token:token})
+                    }
+                })
+              
+            }else{
+                res.status(401);
+                res.json({err: "Credenciais inválidas"});
+            }
+        }else{
+            res.status(404);
+            res.json({err:"O E-mail enviado não existe na base de dados!"})
+        }
+        
+    }else{
+        res.status(400)
+        res.json({err:"O email enviado é invalido"})
     }
 })
 app.listen(3000,()=>{
